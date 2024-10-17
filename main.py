895eebc7c088e21e1279bb36d46ed71b59307be5
Anderson Souza <Anderson1231993@hotmail.com>
2024-10-17 03:55:27 -0300

    refactor(main): implementes scenery class to main

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
+import os
 from sys import exit
 from utils import Dimensions
+from state import Scenery
 
 pygame.init()
 screen = pygame.display.set_mode((Dimensions.SCREEN_WIDTH.value, Dimensions.SCREEN_HEIGHT.value))
@@ -8,6 +10,9 @@ pygame.display.set_caption('Aliens')
 
 clock = pygame.time.Clock()
 FPS = 60
+base_dir = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(base_dir, 'assets', 'images', 'background.jpg')
+background = Scenery(image_path)
 
 
 def game_loop():
@@ -20,6 +25,9 @@ def game_loop():
                 if event.key == pygame.K_ESCAPE:
                     running = False
                     pygame.quit()
+
+        background.update()
+        background.draw(screen)
 
         pygame.display.flip()
         clock.tick(FPS)
