2491afb8ac4bbc99ea161533bdf6e74c53aa6502
Anderson Souza <Anderson1231993@hotmail.com>
2024-10-17 03:54:39 -0300

    feat(state): creates a classe scenery to manage scenario game

---
+++ state/scenery.py
@@ -0,0 +1,20 @@
+import pygame
+from utils import Dimensions as D
+
+
+class Scenery:
+    def __init__(self, image_path):
+        self._background = pygame.image.load(image_path).convert()
+        self._background = pygame.transform.scale(self._background, (D.SCREEN_WIDTH.value, D.SCREEN_HEIGHT.value))
+        self._y = 0
+        self._speed = 0.001
+
+    def update(self):
+        self._y += self._speed
+
+        if self._y > D.SCREEN_HEIGHT.value:
+            self._y = 0
+
+    def draw(self, screen):
+        screen.blit(self._background, (0, self._y))
+        screen.blit(self._background, (0, self._y - D.SCREEN_HEIGHT.value))
