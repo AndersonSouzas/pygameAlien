512e415c671d9c89cd1cc45fc0beac7a3fef0e98
Anderson Souza <Anderson1231993@hotmail.com>
2024-11-22 22:38:02 -0300

    fix(state): update scenery.py to improve scene management

--- state/scenery.py
+++ state/scenery.py
@@ -1,13 +1,20 @@
 import pygame
+import os
 from utils import Dimensions as D
 
 
 class Scenery:
-    def __init__(self, image_path):
-        self._background = pygame.image.load(image_path).convert()
-        self._background = pygame.transform.scale(self._background, (D.SCREEN_WIDTH.value, D.SCREEN_HEIGHT.value))
+    def __init__(self, filename):
+        current_dir = os.path.dirname(os.path.dirname(__file__))
+        assets_dir = os.path.join(current_dir, "assets", "images")
+        self._background_path = os.path.join(assets_dir, filename)
+
+        self._background = pygame.image.load(self._background_path).convert()
+        self._background = pygame.transform.scale(
+            self._background,
+            (D.SCREEN_WIDTH.value, D.SCREEN_HEIGHT.value)
+        )
         self._y = 0
 
     def draw(self, screen):
-        screen.blit(self._background, (0, self._y))
-        screen.blit(self._background, (0, self._y - D.SCREEN_HEIGHT.value))
+        screen.blit(self._background, (0, 0))
