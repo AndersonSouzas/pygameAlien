82270b988b9d66e51c59b471edc33a156ad9964d
Anderson Souza <Anderson1231993@hotmail.com>
2024-12-16 20:07:51 -0300

    chore(sprite): add EnemyManager to __init__.py

--- sprite/__init__.py
+++ sprite/__init__.py
@@ -1,4 +1,4 @@
 from .animation import Animation
-from .enemy import Enemy
+from .enemy import Enemy, EnemyManager
 
-__all__ = ['Animation', 'Enemy']
+__all__ = ['Animation', 'Enemy', 'EnemyManager']
