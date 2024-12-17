c4d9be2e4bdfc66d2e0514fbff4350d87e0f59eb
Anderson Souza <Anderson1231993@hotmail.com>
2024-12-17 11:07:54 -0300

    feat(sprite): implements random enemy sprite selection, uses OS library to robustly access sprite paths, gradually increases enemy speed, changes sprite size and the time interval in which sprites appear

--- sprite/enemy.py
+++ sprite/enemy.py
@@ -1,16 +1,20 @@
 import pygame
 import random
+import os
 from utils import Dimensions as D
 
 
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, x: float, y: float, speed: float):
+    def __init__(self, x: float, y: float, speed_x: float, speed_y: float, sprite_path: str):
         super().__init__()
-        self.image = pygame.image.load("assets/images/sprites/enemys/fishGreen.png").convert_alpha()
+        self.image = pygame.image.load(sprite_path).convert_alpha()
+        self.image = pygame.transform.scale(self.image,
+                                            (int(self.image.get_width() * 0.75),
+                                             int(self.image.get_height() * 0.75)))
         self.rect = self.image.get_rect()
         self.rect.topleft = (x, y)
-        self.speed_x = speed
-        self.speed_y = speed
+        self.speed_x = speed_x
+        self.speed_y = speed_y
         self.screen_width = D.SCREEN_WIDTH.value
         self.screen_height = D.SCREEN_HEIGHT.value
 
@@ -25,20 +29,31 @@ class Enemy(pygame.sprite.Sprite):
 
 
 class EnemyManager:
-    def __init__(self, enemy_group, spawn_interval=2000):
+    def __init__(self, enemy_group, spawn_interval=8000):
         self.spawn_interval = spawn_interval
         self.enemy_group = enemy_group
         self.last_spawn_time = pygame.time.get_ticks()
         self.screen_width = D.SCREEN_WIDTH.value
         self.screen_height = D.SCREEN_HEIGHT.value
+        self.speed_modifier = 1.0
+        self.speed_increment_rate = 0.1
+
+        self.enemy_sprites = [
+            os.path.join("assets", "images", "sprites", "enemies", "ghost.png"),
+            os.path.join("assets", "images", "sprites", "enemies", "spinner.png"),
+        ]
 
     def _generate_random_enemy(self):
         x = random.randint(50, self.screen_width - 50)
         y = random.randint(50, self.screen_height - 50)
-        speed = random.randint(2, 6)
-        return Enemy(x, y, speed)
+        speed_x = random.uniform(2, 4) * random.choice([-1, 1]) * self.speed_modifier
+        speed_y = random.uniform(2, 4) * random.choice([-1, 1]) * self.speed_modifier
+
+        sprite_path = random.choice(self.enemy_sprites)
+
+        return Enemy(x, y, speed_x, speed_y, sprite_path)
         
-    def initial_spawn(self, count=3):
+    def initial_spawn(self, count=2):
         for i in range(count):
             self.enemy_group.add(self._generate_random_enemy())
 
@@ -47,3 +62,4 @@ class EnemyManager:
         if current_time - self.last_spawn_time > self.spawn_interval:
             self.enemy_group.add(self._generate_random_enemy())
             self.last_spawn_time = current_time
+            self.speed_modifier += self.speed_increment_rate
