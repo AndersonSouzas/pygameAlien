7f4ad5d680d29a2fd8d06a51a26fd3bc787b1eaf
Anderson Souza <Anderson1231993@hotmail.com>
2024-12-23 13:32:52 -0300

    refactor(score): creates the render text centered method to render and center text

--- utils/score.py
+++ utils/score.py
@@ -14,36 +14,30 @@ class Score:
         if not self.is_game_over:
             self.score += points
 
+    def _render_text_centered(self, screen: pygame.Surface, text: str, y_offset: int) -> None:
+        screen_width, screen_height = screen.get_size()
+        text_surface = self._custom_font.render_font(
+            text=text,
+            font_size=self.font_size,
+            font_color=self.color,
+            position=(0, 0),
+        )
+        text_rect = text_surface.get_rect(center=(screen_width // 2, screen_height // 2 + y_offset))
+        screen.blit(text_surface, text_rect)
+
     def render_score(self, screen: pygame.Surface) -> None:
-        score_text_surface = self._custom_font.render_font(text=f"Score: {self.score}",
-                                                           font_size=self.font_size,
-                                                           font_color=self.color,
-                                                           position=(10, 10))
+        score_text_surface = self._custom_font.render_font(
+            text=f"Score: {self.score}",
+            font_size=self.font_size,
+            font_color=self.color,
+            position=(10, 10)
+        )
         screen.blit(score_text_surface, (10, 10))
 
     def render_game_over(self, screen: pygame.Surface) -> None:
-
         if self.is_game_over:
-            screen_width, screen_height = screen.get_size()
-
-            game_over_text_surface = self._custom_font.render_font(
-                text=f"Game Over",
-                font_size=self.font_size,
-                font_color=self.color,
-                position=(screen_width // 2, screen_height // 2 - 40)
-            )
-            game_over_rect = game_over_text_surface.get_rect(center=(screen_width // 2, screen_height // 2 - 40))
-
-            final_score_text_surface = self._custom_font.render_font(
-                text=f"Final Score: {self.score}",
-                font_size=self.font_size,
-                font_color=self.color,
-                position=(screen_width // 2, screen_height // 2 + 20)
-            )
-            final_score_rect = final_score_text_surface.get_rect(center=(screen_width // 2, screen_height // 2 + 20))
-
-            screen.blit(game_over_text_surface, game_over_rect)
-            screen.blit(final_score_text_surface, final_score_rect)
+            self._render_text_centered(screen, "Game Over", -40)
+            self._render_text_centered(screen, f"Final Score: {self.score}", 20)
 
     def trigger_game_over(self):
         self.is_game_over = True
