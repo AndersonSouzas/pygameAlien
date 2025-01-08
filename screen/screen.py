88ba4434520c99683d321360905b8c8796b1b087
Anderson Souza <Anderson1231993@hotmail.com>
2025-01-08 15:18:18 -0300

    feat(screen): creates superclass screen

--- screen/screen.py
+++ screen/screen.py
@@ -2,6 +2,7 @@ from enum import Enum
 from pygame import Surface
 from typing import List
 from pygame.event import Event
+from abc import ABC, abstractmethod
 
 
 class ScreenType(Enum):
@@ -9,15 +10,18 @@ class ScreenType(Enum):
     GAME_SCREEN = 'GAME_SCREEN'
 
 
-class Screen:
+class Screen(ABC):
     def __init__(self, screen: Surface):
         self.screen = screen
 
-    def handle_events(self, events: List[Event]):
+    @abstractmethod
+    def handle_events(self, events: List[Event]) -> None:
         pass
 
-    def update(self):
+    @abstractmethod
+    def update(self) -> None:
         pass
 
-    def render(self):
+    @abstractmethod
+    def render(self) -> None:
         pass
